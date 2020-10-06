b22637de840d6876bd76326a25f3a4573f8fffea
sergio núñez meneses <63654322+sergio-nunez-meneses@users.noreply.github.com>
2020-10-06 16:27:32 +0200

    added page detection for pagination and infinite scrolling

--- API/TMDBApi.js
+++ API/TMDBApi.js
@@ -1,7 +1,7 @@
 const API_TOKEN = '5c88f2f1ac2e79214708207ca403f152';
 
-export function getFilmsFromApiWithSearchedText(text) {
-  const url = 'https://api.themoviedb.org/3/search/movie?api_key=' + API_TOKEN + '&language=en&query=' + text;
+export function getFilmsFromApiWithSearchedText(text, page) {
+  const url = 'https://api.themoviedb.org/3/search/movie?api_key=' + API_TOKEN + '&language=en&query=' + text + '&page=' + page;
   return fetch(url)
     .then((response) => response.json())
     .catch((error) => console.error(error));
--- Components/Search.js
+++ Components/Search.js
@@ -7,16 +7,14 @@ import { getFilmsFromApiWithSearchedText } from '../API/TMDBApi' // import { } f
 class Search extends React.Component {
   constructor(props) {
     super (props)
+    this.page = 0
+    this.totalPages = 0
     this.searchedText = ''
     this.state = {
       opacity: 0,
       films: [],
       isLoading: false
     }
-  }
-
-  _searchTextInputChanged(text) {
-    this.searchedText = text
   }
 
   _displayLoading() {
@@ -29,38 +27,59 @@ class Search extends React.Component {
     }
   }
 
+  _searchTextInputChanged(text) {
+    this.searchedText = text
+  }
+
   // JS syntax to indicate that this is a private method
   _loadFilms() {
-    console.log(this.searchedText);
     if (this.searchedText.length > 0) {
       this.setState({ isLoading: true })
-      getFilmsFromApiWithSearchedText(this.searchedText)
+
+      getFilmsFromApiWithSearchedText(this.searchedText, this.page+1)
         .then(data => {
+          this.page = data.page
+          this.totalPages = data.total_pages
           this.setState({
-            films: data.results,
+            // spread syntax
+            films: [...this.state.films, ...data.results],
             isLoading: false
           })
         })
     }
   }
 
+  _searchFilms() {
+    this.page = 0
+    this.totalPages = 0
+    this.setState({ films: [] }, () => {
+      this._loadFilms()
+    })
+  }
+
   render() { // method of the class React.Component
-    console.log('RENDERED');
-    console.log(this.state.isLoading);
     return ( // must return a component for displaying
       <View style={styles.container}>
         <TextInput
           style={styles.textinput}
           placeholder='Enter a film title'
           onChangeText={(text) => this._searchTextInputChanged(text)}
-          onSubmitEditing={() => this._loadFilms()}
+          onSubmitEditing={() => this._searchFilms()}
         />
-        <Button title='Search' onPress={() => this._loadFilms()}/>
+        <Button title='Search' onPress={() => this._searchFilms()}/>
         {/* <FilmItem/> is a custom component */}
         <FlatList
           data={this.state.films}
-          keyExtractor={(item) => item.id.toString()}
-          renderItem={({item}) => <FilmItem film={item}/>}
+          keyExtractor={ (item) => item.id.toString() }
+          renderItem={ ({item}) => <FilmItem film={item}/> }
+          onEndReachedThreshold={0.5}
+          onEndReached={() => {
+              if (this.page < this.totalPages) {
+                // console.log(this.page);
+                this._loadFilms()
+              }
+            }
+          }
         />
         {this._displayLoading()}
       </View>
@@ -92,7 +111,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     left: 0,
     right: 0,
-    top: 100,
+    top: 110,
     bottom: 0,
     backgroundColor: 'rgba(0, 0, 0, 0.8)'
   }
